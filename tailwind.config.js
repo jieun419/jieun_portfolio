7eca3181f8a344eb33defbf36300a30aade0818e
jieun419 <crg1050@naver.com>
2023-08-31 23:28:40 +0900

    style: style code 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,27 @@ export default {
     './src/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mainColorBlack: '#232323',
+        mainColorGray: '#EFEFF1',
+        darkGray: '#0C0C0C',
+        lightGray: '#373737',
+        superLightGray: '#F1F2F5',
+        white: '#fff',
+        black: '#000',
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    ({ addUtilities }) => {
+      addUtilities({
+        '.screen-width': {
+          '@apply mx-auto lg:max-w-[1140px] md:max-w-[720px] sm:max-w-[576px]':
+            '',
+        },
+      });
+    },
+  ],
 }
 
