17a4fc23e4ba40439a593062df05b8096ce4f9e1
jieun419 <crg1050@naver.com>
2023-09-06 21:43:19 +0900

    chore: project, nav 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
+import tw from 'tailwind-styled-components';
+
+import Nav from './components/nav/Nav';
 import Front from './pages/Home/front/Front';
 import Intro from './pages/Home/intro/Intro';
+import Project from './pages/Home/project/Project';
+
+export const Container = tw.main`
+  bg-[#232323]
+  w-full
+  flex
+  p-10
+  gap-32
+`;
+
+export const RightWrap = tw.article`
+  flex-2
+`
 
 export default function App() {
   return (
     <>
       <Front />
-      <Intro/>
+      <Intro />
+      <Container>
+        <Nav />
+        <RightWrap>
+          <Project />
+        </RightWrap>
+      </Container>
     </>
   );
 }
