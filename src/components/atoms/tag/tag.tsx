a5cce841f989485ed5e983fe60dc956281441d40
jieun419 <crg1050@naver.com>
2023-09-06 21:42:35 +0900

    chore: 프로젝트 태그 컴포넌트 마크업

---
+++ src/components/atoms/tag/tag.tsx
@@ -0,0 +1,23 @@
+import tw from 'tailwind-styled-components';
+
+type TagT = {
+  tag: string;
+};
+
+export const TagTxt = tw.span`
+  felx
+  items-center
+  justify-center
+  text-xs
+  text-[#fff]
+  bg-[#232323]
+  px-3
+  py-1
+  rounded
+`;
+
+function TagIcon({ tag }: TagT) {
+  return  <TagTxt>{tag}</TagTxt>
+}
+
+export default TagIcon;
