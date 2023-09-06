ddc1abd360e8a77a77ed2cd3503f83597429971a
jieun419 <crg1050@naver.com>
2023-09-06 21:42:59 +0900

    feat/chore: project 섹션 구체화 작업

---
+++ src/pages/Home/project/Project.tsx
@@ -0,0 +1,29 @@
+import tw from 'tailwind-styled-components';
+import ProjectCard from '../../../components/Cards/ProjectCard';
+
+export const ProjectComponent = tw.article`
+  grid
+  grid-cols-4
+  grid-row-3
+  gap-4
+  h-auto
+  max-md:grid-cols-1
+  max-lg:grid-cols-2
+  max-xl:grid-cols-3
+  max-2xl:grid-cols-4
+`;
+
+function Project() {
+  return (
+    <ProjectComponent>
+      <ProjectCard/>
+      <ProjectCard/>
+      <ProjectCard/>
+      <ProjectCard/>
+      <ProjectCard/>
+      <ProjectCard/>
+    </ProjectComponent>
+  );
+}
+
+export default Project;
