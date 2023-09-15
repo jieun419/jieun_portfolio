4ad4ef473937cb7bdddcc89d73a33080acf4cd3a
jieun419 <crg1050@naver.com>
2023-09-16 03:12:35 +0900

    remove/rename: 데이터 파일 생성

---
+++ src/data/projectData.ts
@@ -0,0 +1,16 @@
+export const projectData = [
+  {
+    name: 'haru',
+    title: 'HARUMATE (하루메이트)',
+    subtext: '당일 일정을 만들고 친구에게 손쉽게 공유할 수 있는 서비스',
+    tag: ['팀 프로젝트', '반응형'],
+    imgurl: '../assets/images/thumbnail_',
+  },
+  {
+    name: 'haru',
+    title: '스택오버플로우',
+    subtext: '스택오버플로우 사이트를 클론 코딩',
+    tag: ['팀 프로젝트', '반응형', '클론코딩', 'UI/UX 변경'],
+    imgurl: '../assets/images/thumbnail_stack.jpg',
+  },
+];
