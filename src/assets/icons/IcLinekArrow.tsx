023e04fb94468fe5f5bd470eedaccf091d3185a9
jieun419 <crg1050@naver.com>
2023-09-19 20:20:33 +0900

    style: 스크롤 유도 컴포넌트 생성

--- src/assets/icons/IcLinekArrow.tsx
+++ src/assets/icons/IcLinekArrow.tsx
@@ -2,18 +2,20 @@ type StyleProps = {
   width: string;
   height: string;
   color?: string;
+  direction?: string;
 }
 
-function IcLineArrow({ width, height, color }: StyleProps) {
+function IcLineArrow({ width, height, color, direction }: StyleProps) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg"
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
       width={width ? width : '15'}
       height={height ? height : '15'}
       viewBox="0 0 28 17"
       fill="none"
     >
       <path
-        d="M2 15L14 3L26 15"
+        d={direction ? direction : 'M2 15L14 3L26 15'}
         stroke={color ? color : 'black'}
         stroke-width="3"
         stroke-linecap="round"
