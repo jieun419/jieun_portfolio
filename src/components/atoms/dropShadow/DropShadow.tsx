d0f1349dc52d40a8c48f11998d389b2ca109994c
jieun419 <crg1050@naver.com>
2023-09-20 01:48:49 +0900

    style: 컴포넌트 분리

---
+++ src/components/atoms/dropShadow/DropShadow.tsx
@@ -0,0 +1,26 @@
+import tw from 'tailwind-styled-components';
+
+type DropShadowProps = {
+  toggleModal: () => void;
+}
+
+export const DropShadowWrap = tw.div`
+  fixed
+  inset-x-0
+  inset-y-0
+  bg-black/80
+  opacity-100
+  z-[-1]
+  backdrop-blur-sm
+`;
+
+
+function DropShadow({ toggleModal }: DropShadowProps) {
+  return (
+    <>
+      <DropShadowWrap onClick={toggleModal} />
+    </>
+  )
+}
+
+export default DropShadow;
