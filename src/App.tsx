274b7099ef795df69be684b6c1bb72856d1197e3
jieun419 <crg1050@naver.com>
2023-09-24 23:49:47 +0900

    refactor: 컴포넌트 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,15 @@
-import tw from 'tailwind-styled-components';
-
-import Nav from './components/nav/Nav';
 import Front from './pages/content/front/Front';
 import Intro from './pages/content/intro/Intro';
-import Project from './pages/content/project/Project';
-import Record from './pages/content/record/Record';
 import GoTopBtn from './components/button/GotoTopBtn';
-import useScrollToTarget from './hooks/useScrollToTarget';
-
-export const Container = tw.main`
-  relative
-  bg-[#232323]
-  w-full
-  flex
-  p-10
-  gap-14
-  max-md:flex-col
-  max-md:gap-0
-  max-ls:gap-5
-  max-xl:gap-10
-`;
-
-export const RightWrap = tw.article`
-  flex
-  flex-col
-  flex-2
-  gap-60
-  pb-20
-`
+import Contents from './pages/content/Contents';
 
 export default function App() {
-
-  const navTabs = [
-    useScrollToTarget('Project'),
-    useScrollToTarget('Record'),
-  ]
-
   return (
     <>
       <GoTopBtn />
       <Front />
       <Intro />
-      <Container>
-        <Nav navTabs={navTabs} />
-        <RightWrap>
-          <Project id='Project' navTabs={navTabs} />
-          <Record id='Record' navTabs={navTabs}/>
-        </RightWrap>
-      </Container>
+      <Contents />
     </>
   );
 }
---
+++ src/pages/content/Contents.tsx
@@ -0,0 +1,47 @@
+import tw from 'tailwind-styled-components';
+
+import Nav from '../../components/nav/Nav';
+import Project from '../content/project/Project';
+import Record from '../content/record/Record';
+import useScrollToTarget from '../../hooks/useScrollToTarget';
+
+export const Container = tw.main`
+  relative
+  bg-[#232323]
+  w-full
+  flex
+  p-10
+  gap-14
+  max-md:flex-col
+  max-md:gap-0
+  max-ls:gap-5
+  max-xl:gap-10
+`;
+
+export const RightWrap = tw.article`
+  flex
+  flex-col
+  flex-2
+  gap-60
+  pb-20
+`
+
+function Contents() {
+
+  const navTabs = [
+    useScrollToTarget('Project'),
+    useScrollToTarget('Record'),
+  ]
+
+  return (
+    <Container>
+      <Nav navTabs={navTabs} />
+      <RightWrap>
+        <Project id='Project' navTabs={navTabs} />
+        <Record id='Record' navTabs={navTabs} />
+      </RightWrap>
+    </Container>
+  );
+}
+
+export default Contents
