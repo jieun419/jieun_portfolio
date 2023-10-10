f645c5a4227071d6e73808f4107acf41ef40f508
jieun419 <crg1050@naver.com>
2023-10-10 21:26:43 +0900

    feat: 스크롤 애니메이션 추가

---
+++ src/styles/ScrollAni.ts
@@ -0,0 +1,37 @@
+import { styled } from 'styled-components';
+
+type Props = {
+  delay?: string;
+};
+
+export const ScrollAni = styled.div<Props>`
+  transition: 0.5s;
+
+  &.fadeIn {
+    opacity: 1;
+  }
+
+  &.fadeOut {
+    opacity: 0;
+  }
+
+  &.fadeAn {
+    animation-name: scroll;
+    animation-duration: 1s;
+    animation-delay: ${(props) => props.delay && props.delay};
+    transform: translateY(0);
+  }
+
+  @keyframes scroll {
+    0% {
+      opacity: 0;
+      transform: translateY(-50px);
+    }
+    100% {
+      opacity: 1;
+      transform: translateY(0);
+    }
+  }
+`;
+
+export default ScrollAni;
