6da6744e03d17c8daa86358a475d771d93d08b65
jieun419 <crg1050@naver.com>
2023-10-10 22:28:45 +0900

    style: 스타일 수정

--- src/pages/content/front/Front.tsx
+++ src/pages/content/front/Front.tsx
@@ -1,4 +1,7 @@
 import { useEffect, useRef } from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../../store';
+import { motion } from 'framer-motion';
 
 import myImg from '../../../assets/images/jieun_img.webp';
 import { MainComponent, TxtWrap, MainTit, StTxt, BgTxt, SubTxt, SocialList, SocialLink, InfoTxtWrap, InfoTxt, Img, ScrollArrowBox } from './Front.styled';
@@ -8,6 +11,7 @@ import { isMobile } from '../../../utils/isMobile';
 
 function Front() {
   const vhRef = useRef(0);
+  const isPosition = useSelector((state: RootState) => state.position.isPosition);
 
   useEffect(() => {
     if (isMobile) {
@@ -17,63 +21,66 @@ function Front() {
   }, []);
 
   return (
-    <MainComponent isMobile={isMobile}>
-      <ScrollArrowBox>
-        <IcLineArrow
-          width='25'
-          height='18'
-          color='#fff'
-          direction='M26 2L14 14L2 2'
+    <motion.main className={`top-0 ${isPosition}`}>
+      <MainComponent isMobile={isMobile}>
+        <ScrollArrowBox>
+          <IcLineArrow
+            width='25'
+            height='18'
+            color='#fff'
+            direction='M26 2L14 14L2 2'
+          />
+          <IcLineArrow
+            width='25'
+            height='18'
+            color='#fff'
+            direction='M26 2L14 14L2 2'
+          />
+        </ScrollArrowBox>
+        <SocialList>
+          <SocialLink href='https://velog.io/@crg1050' title='바로가기(새창)' target='_blank'>Velog</SocialLink>
+          <SocialLink href='https://github.com/jieun419' title='바로가기(새창)' target='_blank'>Github</SocialLink>
+        </SocialList>
+
+        <TxtWrap>
+          <MainTit
+            initial={{ opacity: 0, y: -10 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ ease: "easeOut", duration: 0.8 }}
+          >
+            프론트엔드 개발자 <StTxt>최지은</StTxt>입니다.
+            <BgTxt>Front-End</BgTxt>
+          </MainTit>
+
+          <SubTxt
+            initial={{ opacity: 0, y: -10 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ ease: "easeOut", duration: 0.8, delay: 0.8 }}
+          >
+            사용자를 생각하는 역지사지 마인드,</SubTxt>
+          <SubTxt
+            initial={{ opacity: 0, y: -10 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ ease: "easeOut", duration: 0.8, delay: 1 }}
+          >
+            포기하지 않고 책임감있게 일하는 개발자입니다.</SubTxt>
+        </TxtWrap>
+
+        <Img
+          initial={{ y: 10 }}
+          animate={{ y: 0 }}
+          transition={{ duration: 1 }}
+          src={myImg}
+          isMobile={isMobile}
+          alt="최지은 인물 사진"
         />
-        <IcLineArrow
-          width='25'
-          height='18'
-          color='#fff'
-          direction='M26 2L14 14L2 2'
-        />
-      </ScrollArrowBox>
-      <SocialList>
-        <SocialLink href='https://velog.io/@crg1050' title='바로가기(새창)' target='_blank'>Velog</SocialLink>
-        <SocialLink href='https://github.com/jieun419' title='바로가기(새창)' target='_blank'>Github</SocialLink>
-      </SocialList>
+        <InfoTxtWrap>
+          <InfoTxt>crg1050@gmail.com</InfoTxt>
+          <InfoTxt>update. {UPDATE_DATA}</InfoTxt>
+        </InfoTxtWrap>
+      </MainComponent>
+    </motion.main>
 
-      <TxtWrap>
-        <MainTit
-          initial={{ opacity: 0, y: -10 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ ease: "easeOut", duration: 0.8 }}
-        >
-          프론트엔드 개발자 <StTxt>최지은</StTxt>입니다.
-          <BgTxt>Front-End</BgTxt>
-        </MainTit>
-
-        <SubTxt
-          initial={{ opacity: 0, y: -10 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ ease: "easeOut", duration: 0.8, delay: 0.8 }}
-        >
-          사용자를 생각하는 역지사지 마인드,</SubTxt>
-        <SubTxt
-          initial={{ opacity: 0, y: -10 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ ease: "easeOut", duration: 0.8, delay: 1 }}
-        >
-          포기하지 않고 책임감있게 일하는 개발자입니다.</SubTxt>
-      </TxtWrap>
-
-      <Img
-        initial={{ y: 10 }}
-        animate={{ y: 0 }}
-        transition={{ duration: 1 }}
-        src={myImg}
-        isMobile={isMobile}
-        alt="최지은 인물 사진"
-      />
-      <InfoTxtWrap>
-        <InfoTxt>crg1050@gmail.com</InfoTxt>
-        <InfoTxt>update. {UPDATE_DATA}</InfoTxt>
-      </InfoTxtWrap>
-    </MainComponent>
   )
 }
 
