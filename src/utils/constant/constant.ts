bbd305850f22defd4dc07e08140ab945d2d2b7c1
jieun419 <crg1050@naver.com>
2023-10-15 03:10:38 +0900

    docs: 업데이트 날짜 수정

--- src/utils/constant/constant.ts
+++ src/utils/constant/constant.ts
@@ -1,4 +1,4 @@
 export const ICON_URL =
   'https://raw.githubusercontent.com/jieun419/jieun_portfolio/main/src/assets/icons/icon_';
 
-export const UPDATE_DATA = '23. 10. 13';
+export const UPDATE_DATA = '23. 10. 15';
