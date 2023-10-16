6bc25acfd6c8adfa75ccb93cc0359b7664813dc4
jieun419 <crg1050@naver.com>
2023-10-16 22:01:55 +0900

    docs: 상수 추가

--- src/utils/constant/constant.ts
+++ src/utils/constant/constant.ts
@@ -2,3 +2,17 @@ export const ICON_URL =
   'https://raw.githubusercontent.com/jieun419/jieun_portfolio/main/src/assets/icons/icon_';
 
 export const UPDATE_DATA = '23. 10. 15';
+
+export const OPACITY_0 = { opacity: 0 };
+export const OPACITY_1 = { opacity: 1 };
+
+export const Y_0 = { y: 0 };
+export const Y_M10 = { y: -10 };
+export const Y_P10 = { y: 10 };
+export const Y_M20 = { y: -20 };
+export const Y_P20 = { y: 20 };
+
+export const ANI_Y_OFF = { opacity: 0, y: -10 };
+export const ANI_Y_ON = { opacity: 1, y: 0 };
+
+export const ANI_TRANSITION = { ease: 'easeOut', duration: 0.8 };
