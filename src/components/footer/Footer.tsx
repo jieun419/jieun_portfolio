b84615d20c0411fd3af8476857ab35721a7ea40e
jieun419 <crg1050@naver.com>
2023-10-16 22:20:49 +0900

    style: 불필요한 코드 삭제

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -13,10 +13,6 @@ const FooterContainer = tw.footer`
   justify-center
 `;
 
-const Count = tw.div`
-  
-`;
-
 const FooterTxt = tw.p`
   text-[#A6A6A6]
 `;
@@ -24,9 +20,6 @@ const FooterTxt = tw.p`
 function Footer() {
   return (
     <FooterContainer>
-      <Count>
-        <img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fjieun-portfolio.vercel.app%2Fhit-counter&count_bg=%232323&title_bg=%232323&icon=&icon_color=%23E7E7E7&title=&edge_flat=true" />
-      </Count>
       <FooterTxt>
         Copyright 2023. choijieun all rights reserved.
       </FooterTxt>
