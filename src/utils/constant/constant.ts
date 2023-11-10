2b6948fd6669dc436d3cfb2d358e08d0c5c4d414
jieun419 <crg1050@naver.com>
2023-11-10 19:36:33 +0900

    docs: 오타 및 내용 수정

--- src/utils/constant/constant.ts
+++ src/utils/constant/constant.ts
@@ -1,7 +1,7 @@
 export const ICON_URL =
   'https://raw.githubusercontent.com/jieun419/jieun_portfolio/main/src/assets/icons/icon_';
 
-export const UPDATE_DATA = '23. 11. 07';
+export const UPDATE_DATA = '23. 11. 10';
 export const EMAILL = 'crg1050@gmail.com';
 
 export const OPACITY_0 = { opacity: 0 };
