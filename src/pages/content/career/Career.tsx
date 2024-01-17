bd485fe57643f1d4c5e6646ce1dfa2ee61494c6f
jieun419 <crg1050@naver.com>
2024-01-17 18:01:44 +0900

    feat: 커리어 컨포넌트 생성

---
+++ src/pages/content/career/Career.tsx
@@ -0,0 +1,50 @@
+import tw from 'tailwind-styled-components';
+import RecordCard from '../../../components/cards/RecordCard';
+import { TabsPropsT } from '../../../types/type';
+import { careerData } from '../../../data/content/careerData';
+import useScrollAnimation from '../../../hooks/useScrollAnimation';
+import ScrollAni from '../../../styles/ScrollAni';
+
+
+export const RecordComponent = tw.article`
+  grid
+  grid-cols-4
+  grid-row-3
+  gap-4
+  h-screen
+  pt-[3rem]
+  mt-[3rem]
+    
+  max-md:grid-cols-1
+  max-md:pt-[5rem]
+  max-lg:grid-cols-2
+  max-lg:h-auto
+  max-xl:grid-cols-3
+  max-2xl:grid-cols-4
+  max-2xl:h-screen
+`;
+
+const Career = ({ id, navTabs }: TabsPropsT) => {
+  const { scrollRef, scrollEl } = useScrollAnimation();
+
+  return (
+    <ScrollAni className={`${scrollEl ? 'fadeAn fadeIn' : 'fadeOut'}`} ref={scrollRef}>
+      <RecordComponent id={id} ref={navTabs[0].targetRef}>
+        {
+          careerData.map((item, idx) => (
+            <RecordCard
+              key={idx}
+              data={item.data}
+              title={item.title}
+              role={item.role}
+              infos={item.infos}
+              stacks={item.stacks}
+            />
+          ))
+        }
+      </RecordComponent>
+    </ScrollAni>
+  );
+};
+
+export default Career;
