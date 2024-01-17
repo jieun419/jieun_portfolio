32340a55fd68a9637559076abbac8575297a43b3
jieun419 <crg1050@naver.com>
2024-01-17 18:02:09 +0900

    docs: 내용 수정

--- src/data/content/recordData.ts
+++ src/data/content/recordData.ts
@@ -2,9 +2,9 @@ export const recordData = [
   {
     data: '2023.02 - 2023.08',
     title: '코드스테이츠',
-    role: '@ 프론트엔드 엔지니어 과정 수료',
+    role: '@ 프론트엔드 엔지니어 과정 부트캠프 수료',
     infos: [
-      'JavaScript, React 학습',
+      'JavaScript, React 등 학습',
       '20주 동안 매일 강도 높은 과제 수행 및 페어 프로그래밍과 코드 리뷰 경험',
       '솔로 프로젝트 1회, 협업 프로젝트 2회 진행',
       '스터디에 참여하여 개발 공부를 진행하고 동료들과 원활한 소통과 커뮤니케이션을 촉진',
@@ -12,22 +12,9 @@ export const recordData = [
     stacks: ['html', 'css', 'javascript', 'react', 'github', 'figma'],
   },
   {
-    data: '2021.04 - 2022.09',
-    title: '건국사이버 평생교육원',
-    role: '@ 웹 디자인 / 퍼블리싱',
-    infos: [
-      'Photoshop을 활용해 홍보 랜딩 페이지, 웹 디자인 작업',
-      'HTML, CSS 활용해 자사 및 타사 웹 사이트 구축',
-      '기획자, 프론트엔드 개발자와의 지속적인 커뮤니케이션',
-      '자사 및 타사 웹 사이트 유지보수',
-      '클라이언트와의 직접적인 소통 및 응대',
-    ],
-    stacks: ['html', 'css', 'ps', 'ai'],
-  },
-  {
     data: '2017.05 - 2021.04',
     title: '(주) 비전트리',
-    role: '@ 웹 디자인 / 퍼블리싱',
+    role: '@ 웹 디자인 총괄',
     infos: [
       'Photoshop을 활용해 홍보 랜딩 페이지, 웹 디자인 작업',
       'Photoshop, Illustrator을 활용해 홍보물 디자인 작업',
@@ -36,13 +23,6 @@ export const recordData = [
       '자사 및 타사 웹 사이트 유지보수',
       '클라이언트와의 직접적인 소통 및 응대',
     ],
-    stacks: ['html', 'css', 'ps', 'ai'],
-  },
-  {
-    data: '2015.03 - 2017.02',
-    title: '전주비전대학교',
-    role: '@ 컴퓨터정보과 전문학사 졸업',
-    infos: ['HTML, CSS, JavaScript 등 프로그래밍 언어 학습.', 'CS 전공 학습'],
     stacks: ['html', 'css', 'ps', 'ai'],
   },
 ];
