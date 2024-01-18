fb662d8e1a0a0ee785995afb15ecb1c521756f9c
jieun419 <crg1050@naver.com>
2024-01-19 03:02:15 +0900

    docs: 내용 수정

--- src/data/content/projectData.ts
+++ src/data/content/projectData.ts
@@ -1,14 +1,14 @@
 export const projectData = [
   {
     name: 'ssthing',
-    title: 'SSHTING',
-    subject: '우주가 컨셉인 회사 소개 홈페이지입니다.',
+    title: '회사 소개 사이트',
+    subject: '우주가 컨셉인 회사 소개 사이트입니다.',
     tag: ['팀', '반응형', '퍼블리싱'],
     depoloyurl: 'https://ssting.io/',
   },
   {
     name: 'portfolio',
-    title: '지은 포트폴리오',
+    title: '포트폴리오 사이트',
     subject: '저만의 포트폴리오를 만들기 위해 제작했습니다.',
     tag: ['솔로', '반응형', '포트폴리오'],
     imgurl:
@@ -17,7 +17,7 @@ export const projectData = [
   },
   {
     name: 'haru',
-    title: '하루메이트',
+    title: '당일 일정 등록 서비스',
     subject: '당일 일정을 만들고 친구에게 손쉽게 공유할 수 있는 서비스입니다.',
     tag: ['팀', '반응형'],
     imgurl:
@@ -26,7 +26,7 @@ export const projectData = [
   },
   {
     name: 'stack',
-    title: '스택오버플로우',
+    title: '스택오버플로우 클론 코딩',
     subject:
       '스택오버플로우 사이트를 클론 코딩하며 UI/UX 측면도 고려하며 진행했습니다.',
     tag: ['팀', '반응형', '클론코딩'],
@@ -45,10 +45,16 @@ export const projectData = [
   },
   {
     name: 'hancom',
-    title: '한컴아카데미 온라인',
+    title: 'IT 온라인 수강 사이트',
     subject:
-      'IT융합 전문 교육센터로 기존 홈페이지를 새롭게 리뉴얼한 홈페이지입니다.',
+      'IT융합 전문 교육센터로 기존 사이트를 새롭게 리뉴얼한 사이트입니다.',
     tag: ['장기', '반응형', '퍼블리싱'],
     depoloyurl: 'https://online.hancomacademy.com/',
+  },
+  {
+    name: 'goguma',
+    title: '재능 중개 마켓 플랫폼',
+    subject: '인플루언서의 재능을 거래하는 이커머스 플랫폼 사이트 입니다.',
+    tag: ['장기', '반응형', '디자인', '퍼블리싱'],
   },
 ];
