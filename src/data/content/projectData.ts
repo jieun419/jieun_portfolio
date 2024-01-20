1a88fd842df5163b46775b7208fb2a34fa29c586
jieun419 <crg1050@naver.com>
2024-01-20 22:12:04 +0900

    docs: 프로젝트 4 수정

--- src/data/content/projectData.ts
+++ src/data/content/projectData.ts
@@ -10,7 +10,7 @@ export const projectData = [
     name: 'portfolio',
     title: '포트폴리오 사이트',
     subject: '저만의 포트폴리오를 만들기 위해 제작했습니다.',
-    tag: ['솔로', '반응형', '포트폴리오'],
+    tag: ['솔로', '반응형', '포트폴리오', '사이드'],
     imgurl:
       'https://raw.githubusercontent.com/jieun419/jieun_portfolio/main/src/assets/images/thumbnail_protfolio.webp',
     giturl: 'https://github.com/jieun419/jieun_portfolio',
@@ -19,7 +19,7 @@ export const projectData = [
     name: 'haru',
     title: '당일 일정 등록 서비스',
     subject: '당일 일정을 만들고 친구에게 손쉽게 공유할 수 있는 서비스입니다.',
-    tag: ['팀', '반응형'],
+    tag: ['팀', '반응형', '사이드'],
     imgurl:
       'https://raw.githubusercontent.com/jieun419/jieun_portfolio/main/src/assets/images/thumbnail_haru.webp',
     giturl: 'https://github.com/codestates-seb/seb44_main_006',
@@ -29,7 +29,7 @@ export const projectData = [
     title: '스택오버플로우 클론 코딩',
     subject:
       '스택오버플로우 사이트를 클론 코딩하며 UI/UX 측면도 고려하며 진행했습니다.',
-    tag: ['팀', '반응형', '클론코딩'],
+    tag: ['팀', '반응형', '클론코딩', '사이드'],
     imgurl:
       'https://raw.githubusercontent.com/jieun419/jieun_portfolio/main/src/assets/images/thumbnail_stack.webp',
     giturl: 'https://github.com/codestates-seb/seb44_pre_015',
@@ -38,7 +38,7 @@ export const projectData = [
     name: 'coz',
     title: 'Coz Shopping',
     subject: '주어진 요구 명세서를 확인하며 작업했습니다.',
-    tag: ['솔로'],
+    tag: ['솔로', '사이드'],
     imgurl:
       'https://raw.githubusercontent.com/jieun419/jieun_portfolio/main/src/assets/images/thumbnail_cozshopping.webp',
     giturl: 'https://github.com/jieun419/fe-sprint-coz-shopping',
