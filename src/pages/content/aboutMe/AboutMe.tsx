76012e4a6434c0f9275a4c3b31264e8abf43fbfe
jieun419 <crg1050@naver.com>
2024-01-31 17:21:20 +0900

    feat: 멘트및 배치 수정

--- src/pages/content/aboutMe/AboutMe.tsx
+++ src/pages/content/aboutMe/AboutMe.tsx
@@ -16,6 +16,7 @@ import ScrollAni from '../../../styles/ScrollAni';
 import useScrollAnimation from '../../../hooks/useScrollAnimation';
 import { TagTxt } from '../../../components/atoms/tag/TagTxt';
 import BoldText from '../../../components/atoms/text/BoldText';
+import PointText from '../../../components/atoms/text/PointText';
 
 export const IntroComponent = tw.main`
   relative
@@ -26,23 +27,19 @@ export const IntroComponent = tw.main`
   bg-mainBlack
   px-10
   pt-10
-  h-screen
   z-1
-
-  max-lg:overflow-hidden
   max-lg:h-auto
   max-lg:py-20
 `;
 
 export const Content = tw.section`
   flex
-  flex-wrap
+  flex-col
   gap-20
   items-center
   text-mainGray
   items-center
-  
-  max-lg:flex-col
+  py-20
 `;
 
 export const SubJectTit = tw.h2`
@@ -67,7 +64,7 @@ export const Tit = tw.h3`
 export const SubTit = tw.h5`
   text-xl
   font-bold
-  pb-2
+  pb-6
 `;
 
 export const LeftWrap = tw.div`
@@ -87,7 +84,7 @@ export const RightWrap = tw.div`
 `;
 
 export const Img = tw.img`
-  max-md:w-[80vw]
+  max-md:w-[60vw]
 `;
 
 export const FlexBox = tw.div`
@@ -104,9 +101,21 @@ export const MyMind = tw.div`
   gap-3
 `;
 
-export const MyMindTxt = tw.p`
+export const MyMindTxt = tw.div`
+  relative
   text-base
   mb-5
+  bg-lightGray
+  p-5
+  rounded-tl-2xl
+  rounded-tr-2xl  
+  rounded-bl-2xl
+
+  after:content
+  after:w-full
+  after:block
+  after:bg-[#f0f]
+  after:absolute
 `;
 
 export const StackSpacing = tw.div`
@@ -184,26 +193,24 @@ function AboutMe() {
             </LeftWrap>
             <RightWrap ref={scrollRef}>
               <MyMind>
-
-                <SubTit># 프론트엔드 개발자로서 새로운 도전.</SubTit>
+                <Tit>Interview</Tit>
                 <MyMindTxt>
-                  사용자에게 동적이고 편리한 화면을 제공하고자 프론트엔드 개발자로 전향을 결심하게 되었습니다.<br />
-                  디자인 역량을 바탕으로 <BoldText>사용자와 더 밀착된 상호작용</BoldText> 가능한 프로젝트를 개발하고자 하는 목표와 함께<br /> React, Styled Components, Redux 등 다양한 프레임워크와 라이브러리를 도입해 발전할 수 있는 새로운 도전에 나서고 있습니다.
+                  <SubTit>Q. 프론트엔드로 전향한 이유가 있나요?</SubTit>
+                  5년간 웹 디자이너로 활동하면서 사용자에게 동적이고 편리한 화면을 제공하고자 프론트엔드 개발자로 전향을 결심하게 되었습니다.<br />
+                  디자인 역량을 바탕으로 <PointText>사용자와 더 밀착된 상호작용</PointText> 가능한 프로젝트를 개발하고자 하는 목표와 함께<br />
+                  React, Next.js, Styled Components, Tailwind, Redux, Recoil 등 다양한 프레임워크와 라이브러리를 도입해 발전할 수 있는 새로운 도전에 나서고 있습니다.
                 </MyMindTxt>
 
-                <SubTit># 사용자 중심 개발.</SubTit>
                 <MyMindTxt>
-                  항상 <BoldText>역지사지 마인드로 사용자 중심 개발을 추구합니다.</BoldText> 직관적이고 친숙한 UI 제공과 Lighthouse를 활용해 성능 최적화에 신경쓰면서,<br />
+                  <SubTit>Q. 어떤 마인드로 임하나요?</SubTit>
+                  항상 <PointText>역지사지 마인드로 사용자 중심 개발을 추구합니다.</PointText> 직관적이고 친숙한 UI 제공과 Lighthouse를 활용해 성능 최적화에 신경쓰면서,<br />
                   다양한 사용자들이 편리하게 서비스를 이용 할 수 있도록 기여하고자 합니다.
                 </MyMindTxt>
 
-                <SubTit># 포기하지 않고 배우려는 의지.</SubTit>
                 <MyMindTxt>
+                  <SubTit>Q. 프론트엔드 개발자가 되기 위해 어떤 노력을 하나요?</SubTit>
                   지속적인 학습을 위해 스터디에 적극 참여하고, 다양한 강의를 수강하여 실무에 적용할 수 있는 지식을 쌓고 있습니다.<br />
-                  더불어 <BoldText>낯선 기술에도 포기 하지 않고 메타인지 방식을 활용</BoldText>해 문제에 직면하여 팀원들과의 협업을 통해 더 나은 해결책을 찾고 배우려는 의지가 강합니다.
-                </MyMindTxt>
-
-                <MyMindTxt>
+                  더불어 <PointText>낯선 기술에도 포기 하지 않고 메타인지 방식을 활용</PointText>해 문제에 직면하여 팀원들과의 협업을 통해 더 나은 해결책을 찾고 배우려는 의지가 강합니다.<br />
                   <BoldText>열린 마음으로 피드백을 수용</BoldText>해 더 나은 서비스를 개발하기 위해 노력하며,
                   높은 의지와 열정으로 새로운 도전에  임할 준비가 되어 있습니다.
                 </MyMindTxt>
