d31923f96153ee037aee562e3f90552f2db0e03b
jieun419 <crg1050@naver.com>
2024-04-08 01:46:26 +0900

    feat: router 전역으로 관리하도록 작업

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import Main from './pages/content/main/Main';
 import AboutMe from './pages/content/aboutMe/AboutMe';
 import Contents from './pages/content/Contents';
 import QuickBtns from './components/quickBtns/QuickBtns';
 import Finish from './pages/content/finish/Finish';
+import ProjectDetail from './pages/detail/ProjectDetail';
 
 export default function App() {
   return (
     <>
+      <Routes>
+        <Route path='/project/:name' element={<ProjectDetail />} />
+      </Routes>
       <QuickBtns />
       <Main />
       <AboutMe />
